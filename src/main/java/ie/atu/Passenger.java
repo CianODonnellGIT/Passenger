4945b4ad6366da878942e93ae305a364bf410c90
CianODonnellGIT <G00358872@atu.ie>
2022-11-01 23:41:58 +0000

    getters and setters upgraded

--- src/main/java/ie/atu/Passenger.java
+++ src/main/java/ie/atu/Passenger.java
@@ -1,6 +1,5 @@
 package ie.atu;
 
-import java.util.Scanner;
 
 public class Passenger {
 
@@ -9,6 +8,66 @@ public class Passenger {
     private String title;
     private String name;
     private String phoneNumber;
+
+
+    public Passenger(int age, String id, String title, String name, String phoneNumber ){
+        setAge(age);
+        setId(id);
+        setName(name);
+        setTitle(title);
+        setPhoneNumber(phoneNumber);
+
+    }
+
+    public void setAge(int age){
+        if(age >16){
+            this.age = age;
+        }
+        else
+        {
+            throw new IllegalArgumentException("This is not a valid age");
+        }
+    }
+    public void setId(String id){
+        if(id.length() >10) {
+            this.id = id;
+        }
+        else
+        {
+            throw new IllegalArgumentException("This is not a valid ID");
+        }
+    }
+
+    public void setTitle(String title){
+        if(title.equals("Mr") || title.equals("Mrs") || title.equals("Ms")){
+            this.title = title;
+        }
+        else
+        {
+            throw new IllegalArgumentException("This is not a valid title");
+        }
+
+    }
+    public void setName(String name){
+        if(name.length() > 3 ) {
+            this.name = name;
+        }
+        else
+        {
+            throw new IllegalArgumentException("This is not a valid name");
+        }
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        if(phoneNumber.length() >7) {
+            this.phoneNumber = phoneNumber;
+        }
+        else
+        {
+            throw new IllegalArgumentException("This is not a valid number");
+        }
+    }
+
 
     public int getAge(){
         return age;
@@ -26,66 +85,5 @@ public class Passenger {
         return phoneNumber;
     }
 
-    public void setAge(int validAge){
-        if(age >16){
-            this.age = validAge;
-        }
-        else
-        {
-            throw new IllegalArgumentException("This is not a valid age");
-        }
-    }
-    public void setId(String validId){
-        if(id.length() >10) {
-            this.id = validId;
-        }
-        else
-        {
-            throw new IllegalArgumentException("This is not a valid age");
-        }
-    }
-
-    public void setTitle(String validTitle){
-        if(title.equals("Mr") || title.equals("Mrs") || title.equals("Ms")){
-            this.title = validTitle;
-        }
-        else
-        {
-            throw new IllegalArgumentException("This is not a valid title");
-        }
-
-    }
-    public void setName(String validName){
-        if(name.length() > 3 ) {
-            this.name = validName;
-        }
-        else
-        {
-            throw new IllegalArgumentException("This is not a valid name");
-        }
-    }
-
-    public void setPhoneNumber(String validPhoneNumber){
-        if(phoneNumber.length() >7) {
-            this.phoneNumber = validPhoneNumber;
-        }
-        else
-        {
-            throw new IllegalArgumentException("This is not a valid number");
-        }
-    }
-
-    public Passenger(int validAge){
-
-        if(age >16){
-            age = validAge;
-            System.out.println("Valid age");
-        }
-        else
-        {
-            throw new IllegalArgumentException("This is not a valid age");
-        }
-
-    }
 
 }
